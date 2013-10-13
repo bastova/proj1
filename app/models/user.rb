0f5dd801da730ad016ece8d44f0d6a56403f78ad
DenysBastov <dbastov@gmail.com>
2013-10-13 19:20:04 -0400

    HW6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :like
 	attr_accessible :name, :email, :password
+	validates :name, presence: true
+	validates :name, uniqueness: true
+	emailFormatValidation = /[a-z.-]+@[a-z \d -.]+[.][a-z]+/i
+	validates :email, format: {with: emailFormatValidation}
 end
